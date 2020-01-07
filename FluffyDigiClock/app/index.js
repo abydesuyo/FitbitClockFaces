0efae128194380100c31165af84b32418f4df761
abydesuyo <38089225+abydesuyo@users.noreply.github.com>
2020-01-07 20:58:34 +0900

    Added Transformation for Stats Display

--- FluffyDigiClock/app/index.js
+++ FluffyDigiClock/app/index.js
@@ -7,31 +7,16 @@ import { battery } from "power";
 import * as util from "../common/utils";
 
 
-
 // Update the clock every minute
 clock.granularity = "minutes";
 
 // Get a handle on the <text> element
 const timeDisplay = document.getElementById("timeDisplay");
-// const myStepStats = document.getElementById("myStepStats");
-// const myStairStats = document.getElementById("myStairStats");
-// const myDistStats = document.getElementById("myDistStats");
-// const myActStats = document.getElementById("myActStats");
-// const myBatteryStats = document.getElementById("myBatteryStats");
 
 // Update the <text> element every tick with the current time
 clock.ontick = (evt) => {
   let now = evt.date;
   let hours = now.getHours();
-  // let stepstoday = (today.local['steps'] || 0);
-  // let stepsgoal = (goals['steps'] || 0);
-  // let stairstoday = (today.local['elevationGain'] || 0);
-  // let stairsgoal = (goals['elevationGain'] || 0);
-  // let distoday = Math.round((today.local['distance'] || 0) / 1000);
-  // let distgoal = Math.round((goals['distance'] || 0) /1000);
-  // let activitytoday = (today.local['activeMinutes'] || 0);
-  // let activitygoal = (goals['activeMinutes'] || 0);
-  // let batterystats = (battery['chargeLevel'] || 0);
   
   if (preferences.clockDisplay === "12h") {
     // 12h format
@@ -47,13 +32,6 @@ clock.ontick = (evt) => {
   displayHorizontalStat('activeMinutes','Active');
   displayHorizontalStat('distance','Distance');
   displayBattery();
-  // myStepStats.text = `Steps : ${stepstoday}/${stepsgoal}`;
-  // myStairStats.text = `Stairs : ${stairstoday}/${stairsgoal} floors`;
-  // myDistStats.text = `Distance : ${distoday} km/${distgoal} km`;
-  // myActStats.text = `Activity : ${activitytoday}/${activitygoal}`;
-  // updateHorizontalBar('steps');
-  // updateHorizontalBar('distance');
-  // updateHorizontalBar('calories');
 }
 
 
@@ -73,7 +51,14 @@ function displayHorizontalStat(sTodayStat, displayText)
   let iPercent = Math.floor(iStat/iGoal*100);
   
   var el = document.getElementById(sTodayStat);
-  el.text = `${displayText} : ${iStat}/${iGoal}`;
+  el.text = `${displayText} ${iStat}/${iGoal}`;
+  let width = el.getBBox().width; 
+  let dotstr = '.'
+  while(width < 280){
+    el.text = `${displayText} ${dotstr} ${iStat}/${iGoal}`; 
+    width = el.getBBox().width;
+    dotstr = dotstr + '.';
+  }
   colourScheme(el, iPercent);
 }
 
@@ -118,36 +103,4 @@ function colourScheme(el, iPercentage)
 //   colourStat(el, iPercent);
 // }
 
-
-// function colourStat(el, iPercentage)
-// {
-//   let dtDate = new Date();
-//   let iHours = dtDate.getHours();
-//   let iDayPercent = Math.floor(iHours/24*100);
-
-//   if (iPercentage > 100)
-//   {
-//     el.style.fill = "fb-mint";
-//   }
-//   else
-//   {
-//     let iPercentDiff = iDayPercent - iPercentage;
-//     if (iPercentDiff < 0)
-//     {
-//       el.style.fill = "fb-cyan";
-//     }
-//     else if (iPercentDiff < 10)
-//     {
-//       el.style.fill = "fb-yellow";
-//     }
-//     else if (iPercentDiff < 20)
-//     {
-//       el.style.fill = "fb-orange";
-//     }
-//     else
-//     {
-//       el.style.fill = "fb-red";
-//     }
-//   }
-// }
 
