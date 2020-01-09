daa97c68bdb4ac23904123f52c25e0dd02705d02
abydesuyo <38089225+abydesuyo@users.noreply.github.com>
2020-01-09 19:06:48 +0900

    Stats bars added and package published

--- FluffyPureStats/app/index.js
+++ FluffyPureStats/app/index.js
@@ -1,9 +1,12 @@
 import clock from "clock";
 import document from "document";
 import { preferences } from "user-settings";
+import { today } from "user-activity";
+import { goals } from "user-activity";
+import { battery } from "power";
 import * as util from "../common/utils";
 
-// Update the clock every minute
+// Update the clock every seconds
 clock.granularity = "seconds";
 
 // Get a handle on the <text> element
@@ -35,7 +38,59 @@ clock.ontick = (evt) => {
   mainClock.text = `${hours}:${mins}`;
   secondsClock.text = `${secs}`;
   dateDisplay.text = `${dayoftheWeek}, ${day} ${month} ${year}`;
-  // console.log(`${dayoftheWeek}, ${day} ${month}`);
-  // console.log(`${today.toString()}`);
-  // console.log(`${dayoftheWeek}`)
+  displayBattery();
+  displayHorizontalStat('steps');
+  displayHorizontalStat('elevationGain');
+  displayHorizontalStat('distance');
+  displayHorizontalStat('activeMinutes');
+}
+
+
+function displayHorizontalStat(sTodayStat)
+{
+  let iStat = (today.local[sTodayStat] || 0);
+  let iGoal = (goals[sTodayStat] || 0);
+  let iPercent = Math.floor(iStat/iGoal*100);
+
+  var el = document.getElementById(sTodayStat);
+
+  let widthtoSet = Math.floor(iPercent/100 * 150);
+  if (widthtoSet < 150)
+    {
+      el.width = widthtoSet;      
+    }
+  colourScheme(el, iPercent);
+}
+
+function colourScheme(el, iPercentage)
+{
+  if (iPercentage >= 100)
+  {
+    el.style.fill = "fb-mint";
+  }
+  else if (iPercentage >= 80)
+  {
+    el.style.fill = "fb-cyan";
+  }
+  else if (iPercentage >= 50)
+  {
+    el.style.fill = "fb-green";
+  }
+  else if (iPercentage >= 30)
+  {
+    el.style.fill = "fb-orange";
+  }
+  else
+  {
+    el.style.fill = "fb-red";
+  }
+}
+
+function displayBattery()
+{
+  let batterystats = (battery['chargeLevel'] || 0);
+  var el = document.getElementById('battery');
+  var elstat = document.getElementById('batteryStats');
+  colourScheme(el, batterystats);
+  elstat.text = `${batterystats}%`;
 }
